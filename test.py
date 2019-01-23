24c6a94e23200086ef431893262df96055f28333
Avineshwar Pratap Singh <asingh16@stevens.edu>
2019-01-23 11:53:05 -0800

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import unittest
 
 class mytest(unittest.TestCase):
   def test_otherfilefunction(self):
-    self.assertEqual(sample_to_test(), "sample")
+    self.assertEqual(sample_to_test(), "sample") # this should pass
+    self.assertEqual(sample_to_test(), "Sample") # this should not pass    
 
 if __name__ == '__main__':
   unittest.main()
